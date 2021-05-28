20f3e740af7d9599a81e0d31fb494742d5b95c45
tscheer100 <thomas.k.scheer@gmail.com>
2021-05-27 21:46:13 -0500

    added withdraw/deposit commands

--- cogs/economy.py
+++ cogs/economy.py
@@ -14,7 +14,7 @@ class Economy(commands.Cog):
         print("Economy Cog loaded.")
 
     # Commands
-    @commands.command()
+    @commands.command(aliases = ["bal"])
     async def balance(self, ctx):
         await self.open_account(ctx.author)
         self.user = ctx.author
@@ -30,6 +30,50 @@ class Economy(commands.Cog):
         embed.add_field(name = "Bank balance", value = bank_amt)
         embed.set_footer(icon_url = ctx.author.avatar_url, text = f"requested by {ctx.author.name}")
         await ctx.send(embed = embed)
+
+    @commands.command()
+    async def withdraw(self, ctx, amount = None):
+        await self.open_account(ctx.author)
+        if amount == None:
+            await ctx.send("Please enter the amount")
+            return
+
+        bal = await self.update_bank(ctx.author)
+
+        amount = int(amount)        
+        if amount > bal[1]:
+            await ctx.send("Insufficient amount")
+            return
+        if amount<0:
+            await ctx.send("amount must be positive!")
+            return
+        
+        await self.update_bank(ctx.author, amount)
+        await self.update_bank(ctx.author, -1*amount, "bank")
+
+        await ctx.send(f"You withdrew {amount} coins!")
+
+    @commands.command(aliases = ["dep"])
+    async def deposit(self, ctx, amount = None):
+        await self.open_account(ctx.author)
+        if amount == None:
+            await ctx.send("Please enter the amount")
+            return
+
+        bal = await self.update_bank(ctx.author)
+
+        amount = int(amount)        
+        if amount > bal[0]:
+            await ctx.send("Insufficient amount")
+            return
+        if amount<0:
+            await ctx.send("amount must be positive!")
+            return
+        
+        await self.update_bank(ctx.author, -1*amount)
+        await self.update_bank(ctx.author, amount, "bank")
+
+        await ctx.send(f"You deposited {amount} coins!")
 
     async def open_account(self, user):
         users = await self.get_bank_data()
@@ -51,6 +95,15 @@ class Economy(commands.Cog):
             users = json.load(f)
         return users
 
+    async def update_bank(self, user,change = 0, mode = "wallet"):
+        users = await self.get_bank_data()
+        users[str(user.id)][mode] += change
+
+        with open("bank.json", "w") as f:
+            json.dump(users, f)
+        bal = [users[str(user.id)]["wallet"], users[str(user.id)]["bank"]]
+        return bal
+
     @commands.command()
     async def beg(self, ctx):
         await self.open_account(ctx.author)
@@ -64,6 +117,8 @@ class Economy(commands.Cog):
 
         with open("bank.json", "w") as f:
             json.dump(users, f)
+
+        return self.user
 
 def setup(client):
     client.add_cog(Economy(client))
